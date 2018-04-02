564caaab8cf46f575b6538dd6e36fdc6daa0faee
sassha96 <syvoplias76@gmail.com>
2018-04-02 05:53:03 +0300

    io

--- main.js
+++ main.js
@@ -4,5 +4,15 @@ function test(form) {
     if (form.firstName.value == '') {
         ok1 = false;
         alert("Enter your name!")
+    } else {
+        if ((form.mail.value.indexOf('@', 0) < 1) || (form.mail.value.indexOf('@', 0) == form.mail.value.length - 1)) {
+            ok1 = false
+            alert("Wrong Mail!")
+        } else ok1 = true
+        if (ok1) {
+            //..............
+            alert("Форма заповнена!")
+        }
+
     }
 }
