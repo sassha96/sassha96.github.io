b9f2504295c5af045ecf7e645ff255b921c93a7b
sassha96 <syvoplias76@gmail.com>
2018-04-02 05:58:55 +0300

    io

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function test(form) {
         alert("Enter your name!")
     } else {
         if ((form.mail.value.indexOf('@', 0) < 1) || (form.mail.value.indexOf('@', 0) == form.mail.value.length - 1)) {
-            ok1 = false
+            ok1 = false;
             alert("Wrong Mail!")
         } else ok1 = true
         if (ok1) {
