f79cdf8c41068abbdb352b24a397e5e54f5d3462
sassha96 <syvoplias76@gmail.com>
2018-04-03 21:37:32 +0300

    io

--- register.js
+++ register.js
@@ -1,9 +1,8 @@
-function validateForm() {
+/* function validateForm() {
     var firstName = document.forms["myForm"]["fname"].value;
 
     if (firstName == "") {
         alert("Fields must be filled out!");
         return false;
     }
-}
-
+} */
